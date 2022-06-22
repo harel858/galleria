15e7110b07b845c1fca400ddeecc28318128ba0b
harel858 <harellevi85@gmail.com>
2022-06-22 10:18:23 +0300

    fiinish style

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ import { images } from "./images.js";
 function getImagesTemplate(imageData) {
   let html = ``;
   imageData.forEach((image) => {
-    html += `<div class="card bg-dark text-white">
+    html += `<div class="card bg-dark text-white" id="cardAnimation">
   <img src="${image.getPicture()}" class="card-img" alt="...">
-     <div class="card-img-overlay">
+     <div class="card-img-overlay" >
         <h5 class="card-title">${image.getName()}</h5>
          <h6 class="card-title">${image.getPrice()}</h6>
         <p class="card-text">${image.getDescription()}</p>
